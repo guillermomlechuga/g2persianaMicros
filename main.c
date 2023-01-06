b10ed509b4d07af42e114f74d838d8d4900e7134
apmora <47865396+apmora@users.noreply.github.com>
2023-01-06 21:41:18 +0100

    Versión 2

--- main.c
+++ main.c
@@ -50,6 +50,7 @@ TIM_HandleTypeDef htim2;
 /* USER CODE BEGIN PV */
 volatile int boton_modo=0, p_arriba=0,p_abajo=1,subiendo=0,bajando=0; //Empieza abajo y en el modo manual.
 __IO uint16_t  LDR=0; //Este será el valor al que igualaremos el obtenido por el sensor de luminosidad.
+uint8_t contador=0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -68,8 +69,17 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
 
 	if (GPIO_Pin==GPIO_PIN_0)  //Boton de usuario
 	{
+		if(contador==0)
+		{
 		boton_modo=1;  //SI CAMBIA A AUTOMATICO PERO LUEGO NO VUELVE A MANUAL HAY QUE HACER UN IF Y PONERLO A CERO CUANDO ESTA A 1
-	}
+		contador=1;
+		}
+		else{
+			boton_modo=0;
+			contador=0;
+		}
+
+		}
 
 	if (GPIO_Pin==GPIO_PIN_1)  //Boton subida
 
@@ -138,7 +148,7 @@ int main(void)
 		  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,1);
 		  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,0);
 
-		  for (int i=600;i>0;i=i-30){
+		  for (int i=998;i>=0;i=i-30){
 		  __HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,i);
 		  HAL_Delay(200);}
 		//PERSIANA ARRIBA
@@ -160,7 +170,7 @@ int main(void)
 		  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,0);
 		  HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,0);
 
-	      for (int i=0;i<600;i=i+30){
+	      for (int i=0;i<=998;i=i+30){
 		  __HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,i);
 		  HAL_Delay(200);
 	      }
@@ -190,7 +200,7 @@ int main(void)
    		     HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,1);
     		 HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,0);
 
-    	for (int i=600;i>0;i=i-30){
+    	for (int i=998;i>=0;i=i-30){
         __HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,i);
     	HAL_Delay(200);
     	}
@@ -217,7 +227,7 @@ int main(void)
     	 HAL_GPIO_WritePin(GPIOD,GPIO_PIN_14,0);
     	 HAL_GPIO_WritePin(GPIOD,GPIO_PIN_15,0);
 
-    	 for (int i=0;i<600;i=i+30){ //Se ha bajado ya
+    	 for (int i=0;i<=998;i=i+30){ //Se ha bajado ya
     	 __HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,i);
     	 HAL_Delay(200);}
 
@@ -248,8 +258,9 @@ int main(void)
 
 
 	  }
-  }
+
   /* USER CODE END 3 */
+}
 
 /**
   * @brief System Clock Configuration
@@ -271,7 +282,12 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -281,12 +297,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
@@ -397,9 +413,9 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 0;
+  htim2.Init.Prescaler = 83;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 4294967295;
+  htim2.Init.Period = 999;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
@@ -437,6 +453,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
